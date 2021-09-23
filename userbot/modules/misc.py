afe155c856462c5039ac8671528c38ffcb27f7a8
abdurrohimbontro <89439439+abdurrohimbontro@users.noreply.github.com>
2021-09-23 18:08:55 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -51,7 +51,7 @@ async def randomise(items):
 async def sleepybot(time):
     """ For .sleep command, let the userbot snooze for a few second. """
     counter = int(time.pattern_match.group(1))
-    await time.edit("`Saya tidur dulu tuan Muda...⚡️`")
+    await time.edit("`Saya tidur dulu King...⚡️`")
     if BOTLOG:
         str_counter = time_formatter(counter)
         await time.client.send_message(
@@ -65,23 +65,23 @@ async def sleepybot(time):
 @register(outgoing=True, pattern="^.shutdown$")
 async def killdabot(event):
     """ For .shutdown command, shut the bot down."""
-    await event.edit("`Mematikan Kim-Userbot....`")
+    await event.edit("`Mematikan King-Userbot....`")
     await asyncio.sleep(7)
     await event.delete()
     if BOTLOG:
         await event.client.send_message(BOTLOG_CHATID, "#SHUTDOWN \n"
-                                        "`Kim-Userbot Telah Dimatikan`")
+                                        "`King-Userbot Telah Dimatikan`")
     await bot.disconnect()
 
 
 @register(outgoing=True, pattern="^.restart$")
 async def killdabot(event):
-    await event.edit("`Restarting Kim-Userbot...`")
+    await event.edit("`Restarting King-Userbot...`")
     await asyncio.sleep(10)
     await event.delete()
     if BOTLOG:
         await event.client.send_message(BOTLOG_CHATID, "#RESTARTBOT \n"
-                                        "`Kim-Userbot Telah Di Restart`")
+                                        "`King-Userbot Telah Di Restart`")
     await bot.disconnect()
     # Spin a new instance of bot
     execl(sys.executable, sys.executable, *sys.argv)
@@ -93,50 +93,57 @@ async def killdabot(event):
 async def reedme(e):
     await e.edit(
         "Beberapa Repo Dan File:\n"
-        "\n[🍁𝐊𝐈𝐌 𝐔𝐒𝐄𝐑𝐁𝐎𝐓🍁 Repo](https://github.com/abdurrohimbontro/Kim-Userbot/blob/Kim-Userbot/README.md)"
+        "\n[⚡️𝗞𝗶𝗻𝗴-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡️ Repo](https://github.com/apisuserbot/King-Userbot/blob/King-Userbot/README.md)"
         "\n[Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-11-02)"
         "\n[Setup Guide - Google Drive](https://telegra.ph/How-To-Setup-GDrive-11-02)"
         "\n[Setup Guide - LastFM Module](https://telegra.ph/How-to-set-up-LastFM-module-for-Paperplane-userbot-11-02)"
+        "\n[Video Tutorial - 576p](https://mega.nz/#!ErwCESbJ!1ZvYAKdTEfb6y1FnqqiLhHH9vZg4UB2QZNYL9fbQ9vs)"
+        "\n[Video Tutorial - 1080p](https://mega.nz/#!x3JVhYwR!u7Uj0nvD8_CyyARrdKrFqlZEBFTnSVEiqts36HBMr-o)"
+        "\n[Special - Note](https://telegra.ph/Special-Note-11-02)")
 
-@ register(outgoing=True, pattern="^.repeat (.*)")
+
+@register(outgoing=True, pattern="^.repeat (.*)")
 async def repeat(rep):
-    cnt, txt=rep.pattern_match.group(1).split(' ', 1)
-    replyCount=int(cnt)
-    toBeRepeated=txt
+    cnt, txt = rep.pattern_match.group(1).split(' ', 1)
+    replyCount = int(cnt)
+    toBeRepeated = txt
 
-    replyText=toBeRepeated + "\n"
+    replyText = toBeRepeated + "\n"
 
     for i in range(0, replyCount - 1):
         replyText += toBeRepeated + "\n"
 
     await rep.edit(replyText)
 
-@ register(outgoing=True, pattern="^.repo$")
+
+@register(outgoing=True, pattern="^.repo$")
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
         "╭─━━━━━━━━━━━━━─╮\n"
         "                  Repo\n"
-        "     [🍁𝐊𝐈𝐌 𝐔𝐒𝐄𝐑𝐁𝐎𝐓🍁](https://github.com/abdurrohimbontro/Kim-Userbot)\n"
+        "     [⚡𝗞𝗶𝗻𝗴-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡](https://github.com/apisuserbot/King-Userbot)\n"
         "╭─━━━━━━━━━━━━━─╯\n"
-        "│👨‍💻 **Tuan Muda :** [🍁𝐊𝐈𝐌🍁](t.me/warga_pati)\n"
+        "│👨‍💻 **Mastah :** [Apis](t.me/PacarFerdilla)\n"
         "╰━━━━━━━━━━━━━━━╯\n"
-        "  Deploy King : [Tekan Disini](https://heroku.com/deploy?template=https://github.com/abdurrohimbontro/Kim-Userbot/tree/Kim-Userbot)\n"
-        )
+        "  Tutorial    : [Tutorial Deploy](https://telegra.ph/Deploy-04-06)"
+        "  Deploy King : [Tekan Disini](https://heroku.com/deploy?template=https://github.com/apisuserbot/King-Userbot/tree/King-Userbot)"
+    )
 
-@ register(outgoing=True, pattern="^.raw$")
+
+@register(outgoing=True, pattern="^.raw$")
 async def raw(event):
-    the_real_message=None
-    reply_to_id=None
+    the_real_message = None
+    reply_to_id = None
     if event.reply_to_msg_id:
-        previous_message=await event.get_reply_message()
-        the_real_message=previous_message.stringify()
-        reply_to_id=event.reply_to_msg_id
+        previous_message = await event.get_reply_message()
+        the_real_message = previous_message.stringify()
+        reply_to_id = event.reply_to_msg_id
     else:
-        the_real_message=event.stringify()
-        reply_to_id=event.message.id
+        the_real_message = event.stringify()
+        reply_to_id = event.message.id
     with io.BytesIO(str.encode(the_real_message)) as out_file:
-        out_file.name="raw_message_data.txt"
+        out_file.name = "raw_message_data.txt"
         await event.edit(
             "`Check the userbot log for the decoded message data !!`")
         await event.client.send_file(
@@ -147,40 +154,41 @@ async def raw(event):
             reply_to=reply_to_id,
             caption="`Here's the decoded message data !!`")
 
-@ register(outgoing=True, pattern=r"^.reverse(?: |$)(\d*)")
+
+@register(outgoing=True, pattern=r"^.reverse(?: |$)(\d*)")
 async def okgoogle(img):
     """ For .reverse command, Google search images and stickers. """
     if os.path.isfile("okgoogle.png"):
         os.remove("okgoogle.png")
 
-    message=await img.get_reply_message()
+    message = await img.get_reply_message()
     if message and message.media:
-        photo=io.BytesIO()
+        photo = io.BytesIO()
         await bot.download_media(message, photo)
     else:
-        await img.edit("`Harap Balas Di Gambar`")
+        await img.edit("`Harap Balas Di Gambar King`")
         return
 
     if photo:
         await img.edit("`Processing...`")
         try:
-            image=Image.open(photo)
+            image = Image.open(photo)
         except OSError:
             await img.edit('`Gambar tidak di dukung`')
             return
-        name="okgoogle.png"
+        name = "okgoogle.png"
         image.save(name, "PNG")
         image.close()
         # https://stackoverflow.com/questions/23270175/google-reverse-image-search-using-post-request#28792943
-        searchUrl='https://www.google.com/searchbyimage/upload'
-        multipart={
+        searchUrl = 'https://www.google.com/searchbyimage/upload'
+        multipart = {
             'encoded_image': (name, open(name, 'rb')),
             'image_content': ''
-             }
-        response=requests.post(searchUrl,
+        }
+        response = requests.post(searchUrl,
                                  files=multipart,
                                  allow_redirects=False)
-        fetchUrl=response.headers['Location']
+        fetchUrl = response.headers['Location']
 
         if response != 400:
             await img.edit("`Image successfully uploaded to Google. Maybe.`"
@@ -190,10 +198,10 @@ async def okgoogle(img):
             return
 
         os.remove(name)
-        match=await ParseSauce(fetchUrl +
+        match = await ParseSauce(fetchUrl +
                                  "&preferences?hl=en&fg=1#languages")
-        guess=match['best_guess']
-        imgspage=match['similar_images']
+        guess = match['best_guess']
+        imgspage = match['similar_images']
 
         if guess and imgspage:
             await img.edit(f"[{guess}]({fetchUrl})\n\n`Looking for images...`")
@@ -202,13 +210,13 @@ async def okgoogle(img):
             return
 
         if img.pattern_match.group(1):
-            lim=img.pattern_match.group(1)
+            lim = img.pattern_match.group(1)
         else:
-            lim=3
-        images=await scam(match, lim)
-        yeet=[]
+            lim = 3
+        images = await scam(match, lim)
+        yeet = []
         for i in images:
-            k=requests.get(i)
+            k = requests.get(i)
             yeet.append(k.content)
         try:
             await img.client.send_file(entity=await
@@ -225,35 +233,35 @@ async def okgoogle(img):
 async def ParseSauce(googleurl):
     """Parse/Scrape the HTML code for the info we want."""
 
-    source=opener.open(googleurl).read()
-    soup=BeautifulSoup(source, 'html.parser')
+    source = opener.open(googleurl).read()
+    soup = BeautifulSoup(source, 'html.parser')
 
-    results={'similar_images': '', 'best_guess': ''}
+    results = {'similar_images': '', 'best_guess': ''}
 
     try:
         for similar_image in soup.findAll('input', {'class': 'gLFyf'}):
-            url='https://www.google.com/search?tbm=isch&q=' + \
+            url = 'https://www.google.com/search?tbm=isch&q=' + \
                 urllib.parse.quote_plus(similar_image.get('value'))
-            results['similar_images']=url
+            results['similar_images'] = url
     except BaseException:
         pass
 
     for best_guess in soup.findAll('div', attrs={'class': 'r5a77d'}):
-        results['best_guess']=best_guess.get_text()
+        results['best_guess'] = best_guess.get_text()
 
     return results
 
 
 async def scam(results, lim):
 
-    single=opener.open(results['similar_images']).read()
-    decoded=single.decode('utf-8')
+    single = opener.open(results['similar_images']).read()
+    decoded = single.decode('utf-8')
 
-    imglinks=[]
-    counter=0
+    imglinks = []
+    counter = 0
 
-    pattern=r'^,\[\"(.*[.png|.jpg|.jpeg])\",[0-9]+,[0-9]+\]$'
-    oboi=re.findall(pattern, decoded, re.I | re.M)
+    pattern = r'^,\[\"(.*[.png|.jpg|.jpeg])\",[0-9]+,[0-9]+\]$'
+    oboi = re.findall(pattern, decoded, re.I | re.M)
 
     for imglink in oboi:
         counter += 1
