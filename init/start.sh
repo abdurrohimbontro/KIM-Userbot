9316c3a1f40a9363d48c0226188a97e9c8a3f340
abdurrohimbontro <89439439+abdurrohimbontro@users.noreply.github.com>
2021-09-23 14:39:37 +0700

    Update start.sh

--- init/start.sh
+++ init/start.sh
@@ -1,17 +1,9 @@
-# ----------------------------DEPLOYING-HEROKU---------------------------------->
+#! /usr/bin/env bash
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
 
-
-
-echo "
-         ░██╗░░██╗██╗███╗░░░░░╔███░
-         ░██║░██╔╝██║████╗░░░╔████░
-         ░█████═╝░██║██╔██╗░╔██╗██░
-         ░██╔═██╗░██║██║╚█████╝║██░
-         ░██║░╚██╗██║██║░╚███╝░║██░
-         ░╚═╝░░╚═╝╚═╝╚═╝░░╚═╝░░╚═╝░
-                 USERBOT TELEGRAM
-"
-
+redis-server --daemonize yes
 python3 -m userbot
-
-# ---------------------------END-LOG--------------------------------------------- #
